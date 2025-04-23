e53c1afb31aaab2a0848a889ef047a492d46e095
Pablo <pablo.aqm@gmail.com>
2025-04-23 17:48:40 -0400

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+  ]
+};
